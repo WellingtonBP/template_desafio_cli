be513de7869e7d43a853d6718f78564556d8a73f
Wellington Pacheco <wellingtonpacheco14@gmail.com>
2024-09-10 21:35:33 -0300

    feat: handle persistence

--- lib/desafio_cli/kv_store/persistence.ex
+++ lib/desafio_cli/kv_store/persistence.ex
@@ -11,4 +11,32 @@ defmodule DesafioCli.KvStore.Persistence do
   def init(:no_args) do
     {:ok, nil}
   end
+
+  def write_file(path, content) do
+    GenServer.cast(@me, {:write, path, content})
+  end
+
+  def read_file(path) do
+    GenServer.call(@me, {:read, path})
+  end
+
+  def handle_call({:read, path}, _from_, nil) do
+    reply =
+      path
+      |> File.read()
+      |> case do
+        {:ok, content} ->
+          :erlang.binary_to_term(content)
+
+        _ ->
+          nil
+      end
+
+    {:reply, reply, nil}
+  end
+
+  def handle_cast({:write, path, content}, nil) do
+    File.write(path, :erlang.term_to_binary(content))
+    {:noreply, nil}
+  end
 end
--- lib/desafio_cli/kv_store/shard.ex
+++ lib/desafio_cli/kv_store/shard.ex
@@ -1,4 +1,5 @@
 defmodule DesafioCli.KvStore.Shard do
+  alias DesafioCli.KvStore.Persistence
   alias DesafioCli.Adt.Btree.Tree
   use GenServer, restart: :permanent
 
@@ -7,11 +8,13 @@ defmodule DesafioCli.KvStore.Shard do
   end
 
   def init(state) do
+    Process.send_after(self(), :load_data, 0)
     {:ok, state}
   end
 
   def handle_call({:set, key, value}, _from, %{tree: tree} = state) do
     {status, updated_tree} = Tree.insert(tree, {key, value})
+    send(self(), :sync_data)
     {:reply, status, %{state | tree: updated_tree}}
   end
 
@@ -21,6 +24,16 @@ defmodule DesafioCli.KvStore.Shard do
 
   def handle_cast({:merge, tree_to_merge}, %{tree: tree} = state) do
     updated_tree = Tree.merge(tree, tree_to_merge)
+    send(self(), :sync_data)
     {:noreply, %{state | tree: updated_tree}}
+  end
+
+  def handle_info(:load_data, %{shard_idx: idx} = state) do
+    {:noreply, %{state | tree: Persistence.read_file("shard_#{idx}")}}
+  end
+
+  def handle_info(:sync_data, %{tree: tree, shard_idx: idx} = state) do
+    Persistence.write_file("shard_#{idx}", tree)
+    {:noreply, state}
   end
 end
